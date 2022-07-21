da0d25b02b70e9aadc86aea79d5f4fce2f9df2fc
Laspi <pro43071919@gmail.com>
2022-07-21 22:06:36 +0000

    je

---
+++ file.py
@@ -0,0 +1,4 @@
+from ast import main
+
+
+main(module='__main__', defaultTest=None, argv=None, testRunner=None, testLoader=unittest.defaultTestLoader, exit=True, verbosity=1, failfast=None, catchbreak=None, buffer=None, warnings=None)
